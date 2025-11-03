6198f5cbdd65840614956152beb69580b971d450
ikramsmail <ikrameesmail@gmail.com>
2025-11-03 13:42:49 +0100

    index.html

---
+++ exo1/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// slide 51 Déclaration des constantes
+const nom = "John";
+const prenom = "Doe";
+const age = 30;
+const note = 15;
+const pi = 3.14;
+
+//slide 45 Déclaration de fonctions
+function f1() {
+  console.log("Nom :", nom);
+}
+function f2() {
+  console.log("Prenom :", prenom);
+}
+function f3() {
+  console.log("Nom & Prenom :", nom + "-" + prenom);
+}
+function f4() {
+  console.log("Age :", age);
+}
+function f5() {
+  console.log("Note :", note);
+}
+
+function f6() {
+  console.log("Valeur de pi :", 3.14);
+}
+
